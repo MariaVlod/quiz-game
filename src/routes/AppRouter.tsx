db875a9ceac973ef9d853348d7d06c0d576abb7b
Maria Vlodzianovska <ipz234@student.ztu.edu.ua>
2025-10-11 14:51:17 +0300

    setup routing with react-router-dom

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import StartPage from '../pages/StartPage';
+import GamePage from '../pages/GamePage';
+import ResultPage from '../pages/ResultPage';
+import UserProfilePage from '../pages/UserProfilePage';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+      <Route path="/game" element={<GamePage />} />
+      <Route path="/results" element={<ResultPage />} />
+      <Route path="/user/:id" element={<UserProfilePage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
