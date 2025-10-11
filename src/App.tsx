b47da05630d9f3c3dbb0651c2b5b7728b2a8aa31
Maria Vlodzianovska <ipz234@student.ztu.edu.ua>
2025-10-11 14:53:39 +0300

    integrate AppRouter and remove old page switching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { GameSettingsProvider } from './context/GameSettingsContext';
-import StartPage from './pages/StartPage';
-import GamePage from './pages/GamePage';
-import ResultPage from './pages/ResultPage';
-import type { Question } from './types';
-
-type AppState = 'start' | 'game' | 'results';
-
-const AppContent: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState<AppState>('start');
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [score, setScore] = useState(0);
-  const [answersHistory, setAnswersHistory] = useState<any[]>([]);
-
-  const handleStartGame = (selectedQuestions: Question[]) => {
-    setQuestions(selectedQuestions);
-    setScore(0);
-    setAnswersHistory([]);
-    setCurrentPage('game');
-  };
-
-  const handleEndGame = (finalScore: number, finalAnswersHistory: any[]) => {
-    setScore(finalScore);
-    setAnswersHistory(finalAnswersHistory);
-    setCurrentPage('results');
-  };
-
-  const handleNewGame = () => {
-    setQuestions([]);
-    setScore(0);
-    setAnswersHistory([]);
-    setCurrentPage('start');
-  };
-
-  const handleRestart = () => {
-    setScore(0);
-    setAnswersHistory([]);
-    setCurrentPage('game');
-  };
-
-  return (
-    <div className="app">
-      {currentPage === 'start' && (
-        <StartPage onStart={handleStartGame} />
-      )}
-      {currentPage === 'game' && questions.length > 0 && (
-        <GamePage 
-          questions={questions} 
-          onEndGame={handleEndGame}
-          onNewGame={handleNewGame}
-        />
-      )}
-      {currentPage === 'results' && (
-        <ResultPage 
-          score={score} 
-          answersHistory={answersHistory} 
-          onRestart={handleRestart}
-          onNewGame={handleNewGame}
-        />
-      )}
-    </div>
-  );
-};
+import AppRouter from './routes/AppRouter';
+import './styles/global.css'; // Глобальні стилі
 
 const App: React.FC = () => {
   return (
     <GameSettingsProvider>
-      <AppContent />
+      <Router>
+        <div className="app">
+          <AppRouter />
+        </div>
+      </Router>
     </GameSettingsProvider>
   );
 };
